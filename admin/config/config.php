cd3fa82e12d8c115f58031bf412aa1091a5566b6
hongvinh1608 <153805416+hongvinh1608@users.noreply.github.com>
2024-05-23 19:25:33 +0700

    Update config.php

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("us-cluster-east-01.k8s.cleardb.net","bdb349a88d3d26","6dd0c0f7","heroku_1bbd367bec98762");
+$mysqli = new mysqli("us-cluster-east-01.k8s.cleardb.net","b5e66403615545","b2613ba6","heroku_e9f0d172ad4ec91");
 
 // Check connection
 if ($mysqli -> connect_errno) {
